a5d701a5af83008247911d8afa1094bd0e58fa5b
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-22 20:07:35 +0700

    Update : Endpoint validator

--- src/utils/validators/index.js
+++ src/utils/validators/index.js
@@ -2,11 +2,13 @@ import { registerValidator, loginValidator } from "./validateAuth.js";
 import { changePasswordValidator } from "./validatePassword.js";
 import { updateProfileValidator} from "./validateUser.js";
 import { brandValidator } from "./validateBrand.js";
+import { vehicleValidator } from "./validateVehicle.js";
 
 export { 
     registerValidator, 
     loginValidator, 
     changePasswordValidator, 
     updateProfileValidator,
-    brandValidator
+    brandValidator,
+    vehicleValidator
 };
