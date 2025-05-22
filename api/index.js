ecea67e66fd8e898d6f9144b2cc2ca0969b2e53b
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-23 04:03:42 +0700

    Update : Index

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import cors from "cors";
 import helmet from "helmet";
 import xssClean from "xss-clean";
 import hpp from "hpp";
-import rateLimit from "express-rate-limit";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 
@@ -22,20 +21,6 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS || "*";
 
 app.set('trust proxy', 1);
 
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    standardHeaders: true,
-    legacyHeaders: false,
-    handler: (req, res, next) => {
-        res.status(429).json({
-            status: "fail",
-            message: "Too many requests, please try again later",
-        });
-    },
-});
-
-app.use(limiter);
 app.use(cors({
     origin: allowedOrigins,
     credentials: true
